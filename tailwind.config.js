673ce8d18802fb937a390b12a1626bbf44efc8d6
humashmmalundip-netizen <humashmmalundip@gmail.com>
2026-09-09 16:11:56 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+      colors: {
+        maroon: {
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          200: '#fecdd3',
+          300: '#fda4af',
+          400: '#fb7185',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+          800: '#800020',
+          900: '#580016',
+          950: '#38000d',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
